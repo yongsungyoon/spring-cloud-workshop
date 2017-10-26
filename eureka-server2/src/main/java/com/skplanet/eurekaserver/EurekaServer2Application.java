4187774e9eb7733f5c086e0640e38a2821a5e4b6
Yongsung Yoon <yongsung.yoon@gmail.com>
2017-10-26 20:54:34 +0900

    Be on testing of eureka duplication problem

---
+++ eureka-server2/src/main/java/com/skplanet/eurekaserver/EurekaServer2Application.java
@@ -0,0 +1,14 @@
+package com.skplanet.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer2Application.class, args);
+    }
+}
