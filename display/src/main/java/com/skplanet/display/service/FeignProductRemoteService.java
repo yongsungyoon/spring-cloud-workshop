5420140049152a408c5f1472ceec626dde65569d
Yongsung Yoon <yongsung.yoon@gmail.com>
2019-02-07 16:35:30 +0900

    Upgrade to Spring Boot 2.1.2 and Spring Cloud Greenwich.RELEASE

--- display/src/main/java/com/skplanet/display/service/FeignProductRemoteService.java
+++ display/src/main/java/com/skplanet/display/service/FeignProductRemoteService.java
@@ -1,6 +1,6 @@
 package com.skplanet.display.service;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
